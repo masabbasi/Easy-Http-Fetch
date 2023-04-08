caf93e9b7a973111e0df99351faba45110a756c5
MasAbbasi <masoud.abbasi.71@gmail.com>
2023-04-08 16:51:14 +0330

    All

---
+++ easyhttp.js
@@ -0,0 +1,45 @@
+class EasyHTTP {
+  get(url) {
+    return new Promise((resolve, reject) => {
+      fetch(url)
+        .then((res) => {return res.json();})
+        .then((data) => resolve(data))
+        .catch((err) => reject(err));
+    });
+  }
+
+  post(url, data) {
+    return new Promise((resolve, reject) => {
+      fetch(url,{
+				method:"POST",
+				headers:{"Content-type":"application/json"},
+				body:JSON.stringify(data)
+			})
+			.then(res=>res.json())
+			.then(data=>resolve(data))
+			.catch(err=>reject(err));
+    });
+  }
+
+	put(url, data) {
+    return new Promise((resolve, reject) => {
+      fetch(url,{
+				method:"PUT",
+				headers:{"Content-type":"application/json"},
+				body:JSON.stringify(data)
+			})
+			.then(res=>res.json())
+			.then(data=>resolve(data))
+			.catch(err=>reject(err));
+    });
+  }
+
+	delete(url) {
+    return new Promise((resolve, reject) => {
+      fetch(url)
+        .then((res) => {return res.json();})
+        .then((data) => resolve("User Delete..."))
+        .catch((err) => reject(err));
+    });
+  }
+}
